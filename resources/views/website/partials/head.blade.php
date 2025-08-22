60cc4abd37bacd04372cee187ec56dee49fb3fcf
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-08-22 14:31:04 +0530

    added login / signup btn for enter auth pages

--- resources/views/website/partials/head.blade.php
+++ resources/views/website/partials/head.blade.php
@@ -1,32 +1,35 @@
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Bootslander Bootstrap Template</title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Index - Bootslander Bootstrap Template</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-  <!-- Favicons -->
-  <link href="{{ asset('website/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('website/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="{{ asset('website/assets/img/favicon.png') }}?v={{ time() }}" rel="icon">
+    <link href="{{ asset('website/assets/img/apple-touch-icon.png') }}?v={{ time() }}" rel="apple-touch-icon">
 
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
-  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('website/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('website/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('website/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('website/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('website/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('website/assets/vendor/bootstrap/css/bootstrap.min.css') }}?v={{ time() }}"
+        rel="stylesheet">
+    <link href="{{ asset('website/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}?v={{ time() }}"
+        rel="stylesheet">
+    <link href="{{ asset('website/assets/vendor/aos/aos.css') }}?v={{ time() }}" rel="stylesheet">
+    <link href="{{ asset('website/assets/vendor/glightbox/css/glightbox.min.css') }}?v={{ time() }}"
+        rel="stylesheet">
+    <link href="{{ asset('website/assets/vendor/swiper/swiper-bundle.min.css') }}?v={{ time() }}"
+        rel="stylesheet">
 
-  <!-- Main CSS File -->
-  <link href="{{ asset('website/assets/css/main.css') }}" rel="stylesheet">
-
+    <!-- Main CSS File -->
+    <link href="{{ asset('website/assets/css/main.css') }}?v={{ time() }}" rel="stylesheet">
 </head>
--- resources/views/website/partials/js.blade.php
+++ resources/views/website/partials/js.blade.php
@@ -4,18 +4,16 @@
 
     <!-- Preloader -->
     <div id="preloader"></div>
-
     <!-- Vendor JS Files -->
-    <script src="{{ asset('website/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('website/assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('website/assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('website/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('website/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ asset('website/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('website/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('website/assets/vendor/php-email-form/validate.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('website/assets/vendor/aos/aos.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('website/assets/vendor/glightbox/js/glightbox.min.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('website/assets/vendor/purecounter/purecounter_vanilla.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('website/assets/vendor/swiper/swiper-bundle.min.js') }}?v={{ time() }}"></script>
 
     <!-- Main JS File -->
-    <script src="{{ asset('website/assets/js/main.js') }}"></script>
-
+    <script src="{{ asset('website/assets/js/main.js') }}?v={{ time() }}"></script>
 
     <script>
         // Initialize the map
