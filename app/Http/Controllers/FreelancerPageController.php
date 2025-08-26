5be676eb5022bcddfea748a04ad51e0548ae3879
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-08-26 16:45:32 +0530

    added to dashboardd

--- app/Http/Controllers/FreelancerPageController.php
+++ app/Http/Controllers/FreelancerPageController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Services\Jobs;
 use App\Services\Proposals;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class FreelancerPageController extends Controller
 {
     public function dashboard(Jobs $jobs){
+        Log::info('response data: ',['data '=>$jobs]);
         return view('freelancer.dashboard',['data'=>$jobs->getAllJobs()]);
     }
     public function jobView(Jobs $jobs,$id){
